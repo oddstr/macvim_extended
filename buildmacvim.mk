17b454cb9ee2092321a5ca4b0776b7a8d2c00805
wzvplala <wzvplala@yahoo.com>
2008-12-31 02:44:34 +0900

    modified buildmacvim.{mk,sh} to support migemo

--- buildmacvim.mk
+++ buildmacvim.mk
@@ -1,16 +1,50 @@
 # Makefile to build MacVim.app
-VIM        = src/Vim
-MACVIM_BIN = src/MacVim/build/Release/MacVim.app/Contents/MacOS/MacVim
+VIM           = src/Vim
+MACVIM_BUNDLE = src/MacVim/build/Release/MacVim.app
+MACVIM_BIN    = $(MACVIM_BUNDLE)/Contents/MacOS/MacVim
+VIM_BIN       = $(MACVIM_BUNDLE)/Contents/MacOS/Vim
 
 all: macvim
 
+ifdef APP_WITH_MIGEMO
+MIGEMO_TMP    = src/cmigemo/dist
+MIGEMO_DEST   = $(MACVIM_BUNDLE)/Contents/Resources
+GLOBAL_VIMRC  = $(MACVIM_BUNDLE)/Contents/Resources/vim/vimrc
+LIBMIGEMO     = $(MIGEMO_TMP)/lib/libmigemo.1.dylib
+$(LIBMIGEMO):
+	cd src/cmigemo && ./configure --prefix=`pwd`/dist
+	$(MAKE) -C src/cmigemo/dict utf-8
+	$(MAKE) -C src/cmigemo osx-install
+else
+LIBMIGEMO     = 
+endif
+
+$(VIM): $(LIBMIGEMO)
+	$(MAKE) -C src
+
 macvim: $(MACVIM_BIN)
 
-$(MACVIM_BIN): vim
+$(MACVIM_BIN): $(VIM)
 	cd src/MacVim && xcodebuild
+ifdef APP_WITH_MIGEMO
+	if ! grep migemo $(GLOBAL_VIMRC) &>/dev/null; then \
+	  echo; \
+	  echo '" enable migemo by default'; \
+	  echo 'set migemo'; \
+	  echo 'let &migemodict = fnamemodify($$VIM, ":h") . "/share/migemo/utf-8/migemo-dict"'; \
+	fi >> $(GLOBAL_VIMRC)
+	install -d $(MIGEMO_DEST)/lib
+	install -m 644 $(MIGEMO_TMP)/lib/* $(MIGEMO_DEST)/lib/
+	install -d $(MIGEMO_DEST)/share/migemo/utf-8
+	install -m 644 $(MIGEMO_TMP)/share/migemo/utf-8/* $(MIGEMO_DEST)/share/migemo/utf-8/
+	install_name_tool -change \
+	  "`otool -L $(VIM_BIN) | awk '/libmigemo/ {print $$1}'`" \
+	  "@executable_path/../Resources/lib/libmigemo.1.dylib" \
+	  $(VIM_BIN)
+	install_name_tool -id \
+	  "@executable_path/../Resources/lib/libmigemo.1.dylib" \
+	  "$(MIGEMO_DEST)/lib/libmigemo.1.dylib"
+endif
 	touch $(MACVIM_BIN)
-
-vim:
-	$(MAKE) -C src
 
 # vim: set sw=2:
--- buildmacvim.sh
+++ buildmacvim.sh
@@ -1,5 +1,24 @@
 #! /bin/sh
 
+# migemo が不要ならコメントアウト
+# if you dont want migemo, commentout this.
+#with_migemo=no
+
+# すでにインストール済みの libmigemo を使うときはコメントアウト
+# if you use already-installed libmigemo, commentout this.
+#with_migemo=installed
+
+# migemo が /usr/local 以外の prefix でインストールされているときは以下を設定
+# if you have migemo installed with prefix *other* than /usr/local, set below.
+#with_migemo_prefix=/opt/local
+
+prepare_auto_files() {
+  cp -f src/auto/config.mk.$1 src/auto/config.mk
+  if ! diff src/auto/config.h.$1 src/auto/config.h &>/dev/null; then
+    cp -f src/auto/config.h.$1 src/auto/config.h
+  fi
+}
+
 if which gnumake >/dev/null 2>&1; then
   make=gnumake
 elif which gmake >/dev/null 2>&1; then
@@ -15,6 +34,27 @@ topdir=$(dirname $0)
 test -z "$topdir" && topdir=.
 cd $topdir
 
-exec $make -f buildmacvim.mk macvim
+set -e
+set -x
+case "$with_migemo" in
+  no)
+    prepare_auto_files no_migemo
+    exec $make -f buildmacvim.mk macvim
+    ;;
+  installed)
+    migemo_prefix=${with_migemo_prefix-/usr/local}
+    test -f ${migemo_prefix}/include/migemo.h || \
+      { echo "migemo.h not found in ${migemo_prefix}/include"; exit 1; }
+
+    prepare_auto_files installed_migemo
+    sed -i -e "s.@@@migemo_prefix@@@.${migemo_prefix}." \
+      src/auto/config.mk.installed_migemo
+    exec $make -f buildmacvim.mk macvim
+    ;;
+  *)
+    prepare_auto_files with_migemo
+    exec $make -f buildmacvim.mk macvim APP_WITH_MIGEMO=yes
+    ;;
+esac
 
 # vim: set sw=2 :
